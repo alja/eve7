68c8bae2f6b7707b19812fff2acdcad4cd435afa
alja <amraktadel@ucsd.edu>
2018-03-09 15:14:18 -0800

    align

--- Splitter.controller.js
+++ Splitter.controller.js
@@ -1,43 +1,43 @@
 sap.ui.define(['sap/ui/core/mvc/Controller'],
 	function(Controller) {
-	"use strict";
+	    "use strict";
 
-	var SplitterController = Controller.extend("mysplit.Splitter", {
+	    var SplitterController = Controller.extend("mysplit.Splitter", {
 
 		onInit: function () {
 		    console.log("SPLIT CONTROLLER \n");
 		},
-	    
-		      setMainVerticalSplitterHeight: function(){
-			  var mainViewHeight =screen.height;
-		//	  
-                          var mainViewHeight = document.body.clientHeight;
-//			  console.log(" this view ", this.getView());
-			  var mainToolbarHeight = 49;
-			  var height = mainViewHeight - mainToolbarHeight;
-			  //console.log("height >>>  " , height);
-			  
-			  var splitter =  this.getView().byId("MainAreaSplitter");
-			  // console.log("splitter dddd ", splitter);
-			  if (splitter) {
-                              splitter.setHeight(height + "px");
-                          }
-		      },
-		      onAfterRendering: function(){
-			  var me = this;
-			  setTimeout(
-			      function(){
-				  $(window).on("resize", function(){
-				      me.setMainVerticalSplitterHeight();
-				  });
-				  me.setMainVerticalSplitterHeight();
-			      },
-			      100
-			  );
-			  
-		      }
-	});
+	        
+		setMainVerticalSplitterHeight: function(){
+		    var mainViewHeight =screen.height;
+		    //	  
+                    var mainViewHeight = document.body.clientHeight;
+                    //			  console.log(" this view ", this.getView());
+		    var mainToolbarHeight = 49;
+		    var height = mainViewHeight - mainToolbarHeight;
+		    //console.log("height >>>  " , height);
+		    
+		    var splitter =  this.getView().byId("MainAreaSplitter");
+		    // console.log("splitter dddd ", splitter);
+		    if (splitter) {
+                        splitter.setHeight(height + "px");
+                    }
+		},
+		onAfterRendering: function(){
+		    var me = this;
+		    setTimeout(
+			function(){
+			    $(window).on("resize", function(){
+				me.setMainVerticalSplitterHeight();
+			    });
+			    me.setMainVerticalSplitterHeight();
+			},
+			100
+		    );
+		    
+		}
+	    });
 
-	return SplitterController;
+	    return SplitterController;
 
 });
