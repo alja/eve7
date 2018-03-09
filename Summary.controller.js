8d112bf00f927ae5e5b19d4531f6fda32a33d4c0
alja <amraktadel@ucsd.edu>
2018-03-09 15:13:34 -0800

    add number input

--- Summary.controller.js
+++ Summary.controller.js
@@ -1,15 +1,28 @@
-sap.ui.define(['sap/ui/core/mvc/Controller'],
-	      function(Controller) {
+sap.ui.define(['sap/ui/core/mvc/Controller', "sap/ui/model/json/JSONModel"],
+	      function(Controller, JSONModel) {
 		  "use strict";
 
 		  var SSc = Controller.extend("mysplit.Summary", {
 
+                      amdata : {"number" : "123"},
+
 		      onInit: function () {
 			  console.log("summary controller !!!");
+
+
+                          var oModel = new JSONModel(this.amdata);
+                          this.getView().setModel(oModel);
+
+                          
 		      },
                       requesetNewN:function()
                       {
                           console.log("requesetNewN\n");
+                          globalHandle.Send("changeNumPoints()"); 
+                      },
+                      printEvent:function()
+                      {
+                          console.log("printEvent ", this.amdata.number);
                           globalHandle.Send("changeNumPoints()"); 
                       }
 		      
