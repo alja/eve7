792a4e8d6dc15a8f12dd3d3654e60085b734f9da
alja <amraktadel@ucsd.edu>
2018-04-05 11:32:15 -0700

    Add add GED factory function and base frame

--- Summary.controller.js
+++ Summary.controller.js
@@ -13,11 +13,11 @@ sap.ui.define([
     return Controller.extend("eve.Summary", {
 	onInit: function () {
             
-            	 var data = [
-	     {
-		 fName: "Event"
-	     }
- 	         ];
+            var data = [
+	        {
+		    fName: "Event"
+	        }
+ 	    ];
             
             var oTree = this.getView().byId("tree");
 	    oTree.setMode(sap.m.ListMode.Single);
@@ -25,7 +25,7 @@ sap.ui.define([
             var oModel = new sap.ui.model.json.JSONModel();
             oModel.setData(data);
 	    
-	 oTree.setModel(oModel, "myModelName");
+	    oTree.setModel(oModel, "myModelName");
 
 	    var oStandardTreeItem = new  sap.m.StandardTreeItem({
 	        title: "{myModelName>fName}",
@@ -34,6 +34,36 @@ sap.ui.define([
 	    oTree.bindItems("myModelName>/", oStandardTreeItem);
             this.tree = oTree;
             this.model = oModel;
+
+
+            var gedModel = 
+		{
+	            "ROOT::Experimental::TEvePointSet" : [
+		        {
+			    name : "N points",
+			    function: "changeNpoints",
+			    value : 1
+			},
+		        {
+			    name : "Range",
+			    function: "changeRange",
+			    value : 1
+			}
+	            ],
+	            "ROOT::Experimental::TEveTrack" : [
+		        {
+			    name : "Line width",
+			    function: "change Line Width",
+        		    value : 1
+		        },
+		        {
+			    name : "Fit",
+			    function: "changeFit",
+			    value : true
+			}
+	            ]
+		};
+	    sap.ui.getCore().setModel(this.oProductModel, "ged");
             
 	},
         event: function(lst) {
@@ -54,13 +84,12 @@ sap.ui.define([
             this.model.setData([eventData]);
             this.model.refresh(true);
             this.tree.expandToLevel(3);
-	         sap.ui.getCore().setModel(this.model, "myModelName");
+	    sap.ui.getCore().setModel(this.model, "myModelName");
 
             
-                 this.oProductModel = new sap.ui.model.json.JSONModel();
-	         this.oProductModel.setData([this._event]);
-	         sap.ui.getCore().setModel(this.oProductModel, "event");
-
+            this.oProductModel = new sap.ui.model.json.JSONModel();
+	    this.oProductModel.setData([this._event]);
+	    sap.ui.getCore().setModel(this.oProductModel, "event");
         },
         
         onItemPressed: function(oEvent)
@@ -72,22 +101,59 @@ sap.ui.define([
             this.editorElement = deep_value(this._event, path);
 
             var oProductDetailPanel = this.byId("productDetailsPanel");
-           // var title =   this.editorElement.fName + " (" + this.editorElement._typename + " )" ;
+            // var title =   this.editorElement.fName + " (" + this.editorElement._typename + " )" ;
             var title =  this.editorElement._typename ;
             oProductDetailPanel.setHeaderText(title);
 
             var eventPath = oEvent.getParameter("listItem").getBindingContext("myModelName").getPath();
-          //  eventPath="/arr/1";
+            //  eventPath="/arr/1";
 	    var oProductDetailPanel = this.byId("productDetailsPanel");
             console.log("event path ", eventPath);
 	    oProductDetailPanel.bindElement({ path: eventPath, model: "event" });
-
+        },
+        setupGED:function()
+        {
+            var gedView = this.getView().byId.GED();
+            gedView.destroyContent();
+            
+            var guiDef = this.guiModel[ this.editorElement._typename];
+            for (var i = 0; i < guiDef.length; ++i) {
+                widget = guiDef[1];
+	        switch(typeof this.editorElement._typename) {
+	        case "string":
+		    return new Text(sId, {
+		        text: {
+			    path: "revenue",
+			    type: new StringType()
+		        }
+		    });
+		    
+	        case "number":
+		    return new Input(sId, {
+		        value: {
+			    path: "revenue",
+			    type: new Float()
+		        },
+		        change: function(oEvent) { tc.onInputChange(oEvent);}
+		    });
+		    
+	        case "boolean":
+		    return new CheckBox(sId, {
+		        checked: {
+			    path: "revenue"
+		        },
+		        select: function(event) {
+			    console.log("fff checkbox", event.getSource().getBindingContext().getPath());
+		        }
+		    });
+	        }
+            }
             
         },
         changeNumPoints:function()
         {
             var myJSON = "changeNumPoints(" +  this.editorElement.guid + ", "  + this.editorElement.fN +  ")";
-            sap.ui.getCore().byId("TopEveId").getController().getHandle().Send(myJSON); 
+            sap.ui.getCore().byId("TopEveId").getController().getHandle().Send(myJSON);
         },
 	printEvent: function(event)
         {  
@@ -98,7 +164,7 @@ sap.ui.define([
             var path =  bindingContext.getPath(propertyPath);
             var object =  bindingContext.getObject(propertyPath);
             console.log("obj ",object );
-           // alert("You have change : "+ path + " = " + object + " element " + JSON.stringify(this.editorElement));
+            // alert("You have change : "+ path + " = " + object + " element " + JSON.stringify(this.editorElement));
 
             this.changeNumPoints();
         },
